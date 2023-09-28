d4fcc1414ebdc739c6f930fb00c39cd915bc165b
Prashant C Chaturvedi <pccfrmvns@gmail.com>
2023-09-28 23:48:36 +0530

    Added a test case using scenario simulation

---
+++ income-tax/src/test/java/in/yogakshema/income/TaxCalculationScenarioTest.java
@@ -0,0 +1,8 @@
+package in.yogakshema.income;
+
+import org.junit.runner.RunWith;
+import org.kogito.scenariosimulation.runner.KogitoJunitActivator;
+
+@RunWith(KogitoJunitActivator.class)
+public class TaxCalculationScenarioTest {
+}
